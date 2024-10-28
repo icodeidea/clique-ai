4c0c60fcf82b5c95b8cf2933cc0ce9d0b8392149
LONGYKING <kingsleyonyeneke@gmail.com>
2024-10-28 03:20:35 +0100

    update

--- app/pipelines/supervisor_agent_pipeline.py
+++ app/pipelines/supervisor_agent_pipeline.py
@@ -1,2 +1,18 @@
+from app.workflows.genesis import super_graph
+from langchain_core.messages import HumanMessage
+
+
 def run_agent_pipeline(user_input):
-    pass
+    for s in super_graph.stream(
+            {
+                "messages": [
+                    HumanMessage(
+                        content=user_input
+                    )
+                ],
+            },
+            # Maximum number of steps to take in the graph
+            {"recursion_limit": 150},
+    ):
+        print(s)
+        print("---")
--- app/workflows/genesis.py
+++ app/workflows/genesis.py
@@ -3,14 +3,19 @@ from app.states.supervisor import SupervisorState
 from app.utils.state_helpers import get_last_message, join_graph
 from app.nodes.genesis_supervisor import supervisor_node
 from app.consts import PROJECT_MANAGER, SUPERVISOR, ADMINISTRATOR, FINISH
+from app.models.llms import llm
+from langgraph.checkpoint.memory import MemorySaver
 
+memory = MemorySaver()
 
 super_graph = StateGraph(SupervisorState)
 super_graph.add_node(
-    PROJECT_MANAGER, get_last_message | pm_chain | join_graph
+    # PROJECT_MANAGER, get_last_message | pm_chain | join_graph
+    PROJECT_MANAGER, get_last_message | llm | join_graph
 )
 super_graph.add_node(
-    ADMINISTRATOR, get_last_message | admin_chain | join_graph
+    # ADMINISTRATOR, get_last_message | admin_chain | join_graph
+    ADMINISTRATOR, get_last_message | llm | join_graph
 )
 super_graph.add_node(SUPERVISOR, supervisor_node)
 
@@ -26,4 +31,5 @@ super_graph.add_conditional_edges(
     },
 )
 super_graph.set_entry_point(SUPERVISOR)
-super_graph = super_graph.compile()
+super_graph = super_graph.compile(checkpointer=memory)
+super_graph.get_graph().draw_mermaid_png(output_file_path="clique_genesis_graph.png")
